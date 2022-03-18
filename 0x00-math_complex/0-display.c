965d4c23777db24639480dccaec83cc6818554c3
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-17 21:35:00 -0500

    Task 0 for the general case

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,4 +1,27 @@
 #include "holberton.h"
+
+/**
+ * print_double - print of double value
+ * @b: double number to be printed
+ * Return: nothing
+ */
+void print_double(double b)
+{
+	double b_decimal = b * 10;
+
+	printf("%d", (int)b);
+
+	if ((int)b_decimal % 10 != 0)
+	{
+		printf(".");
+	}
+	while ((int)b_decimal % 10 != 0)
+	{
+		printf("%d", (int)b_decimal % 10);
+		b_decimal *= 10;
+	}
+}
+
 /**
  * display_complex_number - Display a complex number
  * @c: struct complex
@@ -8,20 +31,25 @@
 void display_complex_number(complex c)
 {
 
-		if (c.im == 0)
+		print_double(c.re);
+
+		if (c.im != 0)
 		{
-			printf("%d\n", c.re);
+			if (c.im < 0)
+			{
+				printf(" - ");
+				c.im *= -1;
+			}
+
+			else
+			{
+				printf(" + ");
+			}
+			if (c.im != 1)
+			{
+				print_double(c.im);
+			}
+			printf("i");
 		}
-
-		if (c.im < 0)
-		{
-			printf("%d - %di\n", c.re, -c.im);
-		}
-
-		if (c.im > 0)
-		{
-			printf("%d + %di\n", c.re, c.im);
-		}
-
-
+		printf("\n");
 }

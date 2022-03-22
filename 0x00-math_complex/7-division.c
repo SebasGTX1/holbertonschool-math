365a3ae96d9113c26a56808c5781784bfaaf75a8
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-22 08:32:17 -0500

    Revert "Signe correction"
    This reverts commit 98469ee7a2857e0b3c3b479bbf4c838ab3ff1d33.

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -13,6 +13,6 @@ void division(complex c1, complex c2, complex *c3)
 	double den = (c2.re * c2.re) + (c2.im * c2.im);
 
 	c3->re = ((c1.re * c2.re) + (c1.im * c2.im)) / den;
-	c3->im = ((c1.re * c2.im) - (c2.re * c1.im)) / den;
+	c3->im = ((c1.re * -c2.im) + (c2.re * c1.im)) / den;
 }
 

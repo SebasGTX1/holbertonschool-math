8923f355ea002e522a1851dbda14bbf2909e8255
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-22 15:14:58 -0500

    Updating files

---
+++ 0x02-math_integrals_and_ode/euler.c
@@ -0,0 +1,20 @@
+#include "euler.h"
+/**
+* Euler - Code the Euler Method
+* @euler: euler parameter
+* @name: name parameter
+* @x0: x(0)
+* @y0: y(0)
+* @T: T parameter
+* @n: n parameter
+* Return: aproximation to the integral value
+*/
+void Euler(FILE *euler, char *name, double x0, double y0, double T, int n)
+{
+	UNUSED(euler);
+	UNUSED(name);
+	UNUSED(x0);
+	UNUSED(y0);
+	UNUSED(T);
+	UNUSED(n);
+}
---
+++ 0x02-math_integrals_and_ode/euler.h
@@ -0,0 +1,8 @@
+#ifndef _EULER_
+#define _EULER_
+#define UNUSED(x) (void)(x)
+#include <stdio.h>
+
+void Euler(FILE *euler, char *name, double x0, double y0, double T, int n);
+
+#endif

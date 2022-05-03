61cc41ff921b1afcfbeedfe6799adf06085d4680
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-03 17:16:56 -0500

    False compariton fixing

--- 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def gaussian_elimination(A, b):
-    if np.linalg.det(A) is False:
+    if not np.linalg.det(A):
         print("You can't divide by zero!")
         return None
     return np.linalg.solve(A, b)

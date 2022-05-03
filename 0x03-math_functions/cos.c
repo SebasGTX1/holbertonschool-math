69c974bd2838f8432916f4fda85e5d50f05abc98
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-03 17:45:45 -0500

    Task 0

---
+++ 0x03-math_functions/cos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+/**
+ * main - Fucntion tha calculate the slope of cos
+ * Return: no return
+ */
+int main(void)
+{
+	FILE *fp = NULL;
+	double x, y;
+
+	fp = fopen("cos(x).txt", "w");
+	for (x = 0.0000; x <= 6 * M_PI; x = x + 0.001)
+	{
+		y = -(1) * sin(x);
+		fprintf(fp, "%lf\t%lf\n", x, y);
+	}
+	return (0);
+}

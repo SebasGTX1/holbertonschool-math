3d721de876bbc29679a00d0bd01e6e4dada79aad
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-13 21:17:54 -0500

    Adding task 0 to 2

---
+++ 0x05-Math_LU_Factorization/0-lu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def lu(A):
+    size = len(A)
+    L = np.eye(size, dtype=np.double)
+    U = np.array(A, np.double)
+    for i in range(size):
+        fact = U[i + 1:, i] / U[i, i]
+        L[i + 1:, i] = fact
+        U[i + 1:, ] -= fact[:, np.newaxis] * U[i]
+    return L, U
